5e032220b76b38f9cfa8423de9b74acc706c6aae
Jesteena-Jose <112373198+Jesteena-Jose@users.noreply.github.com>
2022-10-31 11:11:23 +0530

    Task 2 commit (#1)

--- src/app/Components/objects-palette/objects-palette.component.ts
+++ src/app/Components/objects-palette/objects-palette.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'app-objects-palette',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObjectsPaletteComponent implements OnInit {
 
+  canvas:any
   constructor() { }
 
   ngOnInit(): void {
+    this.canvas = new fabric.Canvas('canvas');
+  }
+
+  drawRectangle() {
+    var rect = new fabric.Rect({
+      left:Math.random() * this.canvas.width,
+      top: Math.random() * this.canvas.height,
+      fill: 'red',
+      width: 60,
+      height: 30
+    });
+    this.canvas.add(rect)
+  }
+  drawTriangle() {
+    var triangle = new fabric.Triangle({
+      width: 50,
+      height: 70,
+      fill: 'blue',
+      left: Math.random() * this.canvas.width,
+      top: Math.random() * this.canvas.height
+    });
+    this.canvas.add(triangle);
+  }
+  drawCircle() {
+    var circle = new fabric.Circle({
+      radius: 40,
+      fill: 'green',
+      left:Math.random() * this.canvas.width,
+      top: Math.random() * this.canvas.height
+    });
+    this.canvas.add(circle);
   }
 
 }
