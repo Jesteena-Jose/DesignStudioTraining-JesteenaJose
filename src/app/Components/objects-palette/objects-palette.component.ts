7dc0ea3af0e36452238e6e15f47cb658cf4b1ded
Jesteena Jose <jesteena.jose@soti.net>
2022-11-01 19:11:05 +0530

    Task 3 commit

--- src/app/Components/objects-palette/objects-palette.component.ts
+++ src/app/Components/objects-palette/objects-palette.component.ts
@@ -8,30 +8,58 @@ import { fabric } from 'fabric';
 })
 export class ObjectsPaletteComponent implements OnInit {
 
-  canvas:any
+  canvas: any
+  shapes: any
   constructor() { }
 
   ngOnInit(): void {
     this.canvas = new fabric.Canvas('canvas');
+    let myEventInspector = <HTMLElement>document.querySelector("#event");
+    this.shapes = { 'rect': 'Rectangle', 'triangle': 'Triangle', 'circle': 'Circle' }
+
+    // object add
+    this.canvas.on('object:added', (options: any) => {
+      if (options.target) {
+        myEventInspector.innerHTML = '<p>' + this.shapes[options.target.type] + ' was added' + '</p>';
+      }
+    });
+    //object translate
+    this.canvas.on('object:moving', (options: any) => {
+      if (options.target) {
+        myEventInspector.innerHTML = '<p>' + this.shapes[options.target.type] + ' was translated' + '</p>';
+      }
+    });
+    //object scale
+    this.canvas.on('object:scaling', (options: any) => {
+      if (options.target) {
+        myEventInspector.innerHTML = '<p>' + this.shapes[options.target.type] + ' was scaled' + '</p>';
+      }
+    });
+    //object rotate
+    this.canvas.on('object:rotating', (options: any) => {
+      if (options.target) {
+        myEventInspector.innerHTML = '<p>' + this.shapes[options.target.type] + ' was rotated' + '</p>';
+      }
+    });
   }
 
   drawRectangle() {
-    var rect = new fabric.Rect({
-      left:Math.random() * this.canvas.width,
-      top: Math.random() * this.canvas.height,
+    var rectangle = new fabric.Rect({
+      left: 100 + Math.random() * this.canvas.width * 0.25,
+      top: 100 + Math.random() * this.canvas.height * 0.25,
       fill: 'red',
       width: 60,
       height: 30
     });
-    this.canvas.add(rect)
+    this.canvas.add(rectangle)
   }
   drawTriangle() {
     var triangle = new fabric.Triangle({
       width: 50,
       height: 70,
       fill: 'blue',
-      left: Math.random() * this.canvas.width,
-      top: Math.random() * this.canvas.height
+      left: 200 + Math.random() * this.canvas.width * 0.25,
+      top: 400 + Math.random() * this.canvas.height * 0.25
     });
     this.canvas.add(triangle);
   }
@@ -39,8 +67,8 @@ export class ObjectsPaletteComponent implements OnInit {
     var circle = new fabric.Circle({
       radius: 40,
       fill: 'green',
-      left:Math.random() * this.canvas.width,
-      top: Math.random() * this.canvas.height
+      left: 400 + Math.random() * this.canvas.width * 0.25,
+      top: 10 + Math.random() * this.canvas.height * 0.25
     });
     this.canvas.add(circle);
   }
