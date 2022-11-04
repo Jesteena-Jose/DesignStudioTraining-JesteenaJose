230cdc5a7024e3d44b0e62cf474b7a78f1eb8520
Jesteena Jose <jesteena.jose@soti.net>
2022-11-04 13:13:06 +0530

    Add datatype

--- src/app/Components/objects-palette/objects-palette.component.ts
+++ src/app/Components/objects-palette/objects-palette.component.ts
@@ -4,31 +4,28 @@ import { CanvasServiceService } from 'src/app/Services/canvas-service.service';
 import { EventServiceService } from 'src/app/Services/event-service.service';
 
 @Component({
-  selector: 'app-objects-palette',
-  templateUrl: './objects-palette.component.html',
-  styleUrls: ['./objects-palette.component.css']
+    selector: 'app-objects-palette',
+    templateUrl: './objects-palette.component.html',
+    styleUrls: ['./objects-palette.component.css'],
 })
 export class ObjectsPaletteComponent implements OnInit {
+    canvas!: fabric.Canvas;
+    constructor(private canvasservice: CanvasServiceService, private eventservice: EventServiceService) {}
 
-  canvas: any
-  shapes: any
-  constructor(private canvasservice: CanvasServiceService, private eventservice: EventServiceService) { }
+    ngOnInit(): void {
+        this.canvas = new fabric.Canvas('canvas');
+        this.canvasservice.canvas = this.canvas;
+        this.eventservice.canvas = this.canvas;
+        this.eventservice.eventHandler();
+    }
 
-  ngOnInit(): void {
-    this.canvas = new fabric.Canvas('canvas');
-    this.canvasservice.canvas = this.canvas;
-    this.eventservice.canvas = this.canvas;
-    this.eventservice.eventHandler()
-  }
-
-  drawRectangle() {
-    this.canvasservice.drawRectangle()
-  }
-  drawTriangle() {
-    this.canvasservice.drawTriangle()
-  }
-  drawCircle() {
-    this.canvasservice.drawCircle()
-  }
-
+    drawRectangle() {
+        this.canvasservice.drawRectangle();
+    }
+    drawTriangle() {
+        this.canvasservice.drawTriangle();
+    }
+    drawCircle() {
+        this.canvasservice.drawCircle();
+    }
 }
--- src/app/Services/event-service.service.ts
+++ src/app/Services/event-service.service.ts
@@ -2,44 +2,42 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class EventServiceService {
+    canvas!: fabric.Canvas;
+    public subject = new BehaviorSubject<string>('');
+    constructor() {}
 
-  shapes:any
-  canvas:any
-  public subject=new BehaviorSubject<string>("");
-  constructor() { }
+    eventHandler() {
+        let shapes = { rect: 'Rectangle', triangle: 'Triangle', circle: 'Circle' };
+        // object add
+        this.canvas.on('object:added', (options: any) => {
+            if (options.target) {
+                this.subject.next(shapes[options.target.type as keyof typeof shapes] + ' was added');
+            }
+        });
+        //object translate
+        this.canvas.on('object:moving', (options: any) => {
+            if (options.target) {
+                this.subject.next(shapes[options.target.type as keyof typeof shapes] + ' was translated');
+            }
+        });
+        //object scale
+        this.canvas.on('object:scaling', (options: any) => {
+            if (options.target) {
+                this.subject.next(shapes[options.target.type as keyof typeof shapes] + ' was scaled');
+            }
+        });
+        //object rotate
+        this.canvas.on('object:rotating', (options: any) => {
+            if (options.target) {
+                this.subject.next(shapes[options.target.type as keyof typeof shapes] + ' was rotated');
+            }
+        });
+    }
 
-  eventHandler(){
-    this.shapes = { 'rect': 'Rectangle', 'triangle': 'Triangle', 'circle': 'Circle' }
-    // object add
-    this.canvas.on('object:added', (options: any) => {
-      if (options.target) {
-        this.subject.next(this.shapes[options.target.type]+ ' was added')
-      }
-    });
-    //object translate
-    this.canvas.on('object:moving', (options: any) => {
-      if (options.target) {
-        this.subject.next(this.shapes[options.target.type]+ ' was translated')
-      }
-    });
-    //object scale
-    this.canvas.on('object:scaling', (options: any) => {
-      if (options.target) {
-        this.subject.next(this.shapes[options.target.type]+ ' was scaled')
-      }
-    });
-    //object rotate
-    this.canvas.on('object:rotating', (options: any) => {
-      if (options.target) {
-        this.subject.next(this.shapes[options.target.type]+ ' was rotated')
-      }
-    });
-  }
-
-  eventMessage():Observable<string>{
-    return this.subject.asObservable();
-  }
+    eventMessage(): Observable<string> {
+        return this.subject.asObservable();
+    }
 }
