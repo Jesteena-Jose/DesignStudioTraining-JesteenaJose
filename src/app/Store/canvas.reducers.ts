a12e8dbe852f48e9adff853640a679347bdc4dbd
Jesteena Jose <jesteena.jose@soti.net>
2022-11-21 16:30:08 +0530

    remove switch statement

--- src/app/Store/canvas.reducers.ts
+++ src/app/Store/canvas.reducers.ts
@@ -2,22 +2,13 @@ import { State, initialState } from './canvas.state';
 import { ActionTypes, CanvasActions } from './canvas.actions';
 
 export function canvasReducer(state = initialState, action: CanvasActions): State {
-    switch (action.type) {
-        case ActionTypes.UpdateCanvas:
-            return {
-                ...state,
-                canvasState: action.payload.canvasState,
-                canvasActionType: action.payload.canvasActionType,
-                CanvasBool: action.payload.CanvasBool,
-            };
-        case ActionTypes.UndoRedoCanvasBoolean:
-            return {
-                ...state,
-                canvasState: action.payload.canvasState,
-                canvasActionType: action.payload.canvasActionType,
-                CanvasBool: action.payload.CanvasBool,
-            };
-        default:
-            return state;
+    if (action.type === ActionTypes.UpdateCanvas || action.type === ActionTypes.UndoRedoCanvasBoolean) {
+        return {
+            ...state,
+            canvasState: action.payload.canvasState,
+            canvasActionType: action.payload.canvasActionType,
+            CanvasBool: action.payload.CanvasBool,
+        };
     }
+    return state;
 }
